9b2a7f1597f07e33691f5ee06376ecb4db93e939
malu ivo <142180813+mavoruni@users.noreply.github.com>
2024-10-05 11:30:19 -0300

    Add files via upload

---
+++ header.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.cdnfonts.com/css/nekluch-trial" rel="stylesheet">
+    <link href="https://fonts.cdnfonts.com/css/more-sugar" rel="stylesheet">
+    <link href="https://fonts.cdnfonts.com/css/too-sweets" rel="stylesheet">
+    <link href="https://fonts.cdnfonts.com/css/adlam-display" rel="stylesheet">
+                
+    <link rel="stylesheet" href="style.css">
+    <title>EduLab</title>
+</head>
+
+<body>
+    <header>
+        <h1><a href="index.html">EduLab</a></h1>
+        <nav>
+            <ul>
+                <li>
+                    <button class="botao"><a href="#">Informática</a></button>
+                </li>
+                <li class="dropdown">
+                    <button class="botao"><a href="portugues.html">Português</a></button>
+                    <div class="dd-menu">
+                        <a href="portugues.html">Tudo</a>
+                        <a href="#">1° ano</a>
+                        <a href="#">2° ano</a>
+                        <a href="#">3° ano</a>
+                        <a href="#">4° ano</a>
+                        <a href="#">5° ano</a>
+                    </div>
+                </li>
+                <li class="dropdown">
+                    <button class="botao"><a href="matematica.html">Matemática</a></button>
+                    <div class="dd-menu">
+                        <a href="matematica.html">Tudo</a>
+                        <a href="#">1° ano</a>
+                        <a href="#">2° ano</a>
+                        <a href="#">3° ano</a>
+                        <a href="#">4° ano</a>
+                        <a href="#">5° ano</a>
+                    </div>
+                </li>
+                <li class="dropdown">
+                    <button class="botao"><a href="ciencias.html">Ciências</a></button>
+                    <div class="dd-menu">
+                        <a href="ciencias.html">Tudo</a>
+                        <a href="#">1° ano</a>
+                        <a href="#">2° ano</a>
+                        <a href="#">3° ano</a>
+                        <a href="#">4° ano</a>
+                        <a href="#">5° ano</a>
+                    </div>
+                </li>
+                <li class="dropdown">
+                    <button class="botao"><a href="historia.html">História</a></button>
+                    <div class="dd-menu">
+                        <a href="historia.html">Tudo</a>
+                        <a href="#">1° ano</a>
+                        <a href="#">2° ano</a>
+                        <a href="#">3° ano</a>
+                        <a href="#">4° ano</a>
+                        <a href="#">5° ano</a>
+                    </div>
+                </li>
+                <li class="dropdown">
+                    <button class="botao"><a href="geografia.html">Geografia</a></button>
+                    <div class="dd-menu">
+                        <a href="geografia.html">Tudo</a>
+                        <a href="#">1° ano</a>
+                        <a href="#">2° ano</a>
+                        <a href="#">3° ano</a>
+                        <a href="#">4° ano</a>
+                        <a href="#">5° ano</a>
+                    </div>
+                </li>
+                <li>
+                    <button class="botao"><a href="#">Arte</a></button>
+                </li>
+            </nav>
+    </header>
+</body>
